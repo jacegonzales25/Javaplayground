1bd55b88153b00cb6ac4583c7cf26f1cd5129d93
Jace Gonzales <jacegonzales25@gmail.com>
2023-05-14 20:30:54 +0800

    Added public void game starter, toUpperMoves, checks if the moves are equal

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -6,30 +6,37 @@ public class RockPaperScissors{
 
     public static void main(String[] args){
 
+
+
+
+
+    }
+
+    public void game(){
+        
         System.out.println("Welcome to Rock, Paper, Scissors!");
 
 
-
-
-    }
-
-    public boolean isequalmove() {
-
-
-
-        return true;
     }
 
     public String getPlayer1move(){
+        System.out.println("Enter Player 1 move: ");
         String player1move = scan.nextLine();
-        return player1move;
+        return player1move.toUpperCase();
     }
 
     public String getPlayer2move(){
+        System.out.println("Enter Player 2 move: ");
         String player2move = scan.nextLine();
-        return player2move;
+        return player2move.toUpperCase();
     }
 
+    public boolean isequalmove(String player1move, String player2move) {
+        if (player1move.equals(player2move)){
+            return true;
+        }
 
+        return false;
+    }
 
 }
