48b84a7d3fde922aef7b9ce82daac39e19c01e09
Jace Gonzales <jacegonzales25@gmail.com>
2023-05-14 20:49:22 +0800

    "Added recursion and checking of valid moves"

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -20,14 +20,24 @@ public class RockPaperScissors{
     }
 
     public String getPlayer1move(){
+        System.out.println("Player 1 enter your choice: R/P/S");
         System.out.println("Enter Player 1 move: ");
         String player1move = scan.nextLine();
+        if(!player1move.equals("R") && !player1move.equals("P") && !player1move.equals("S")){
+            System.out.println("Enter valid move!");
+            getPlayer1move();
+        }
         return player1move.toUpperCase();
     }
 
     public String getPlayer2move(){
+        System.out.println("Player 2 enter your choice: R/P/S");
         System.out.println("Enter Player 2 move: ");
         String player2move = scan.nextLine();
+        if(!player2move.equals("R") && !player2move.equals("P") && !player2move.equals("S")){
+            System.out.println("Enter valid move!");
+            getPlayer2move();
+        }
         return player2move.toUpperCase();
     }
 
