652858caa00e20e5d65954782f35b87c107ff6fa
Jace Gonzales <jacegonzales25@gmail.com>
2023-05-17 10:01:27 +0800

    Added recursive algorithm for number of requests and starting point userinput

--- diskManagementFCFS.java
+++ diskManagementFCFS.java
@@ -13,11 +13,19 @@ public class diskManagementFCFS{
 
     static int[] getUserInput(){
         Scanner userInput = new Scanner(System.in);
+        System.out.println("The system has a total disk of 200 head");
         System.out.println("Enter number of requests: ");
         
         int requests_num = userInput.nextInt();
         int[] requests_array = new int[requests_num + 1];
 
+        if (requests_num < 0 || requests_num > 199){
+            System.out.println("Error! Input any integer greater than 0");
+            getUserInput();
+        }
+
+        System.out.println("Enter starting point: ");
+        int starting_num = userInput.nextInt();  
 
         return requests_array;
 
