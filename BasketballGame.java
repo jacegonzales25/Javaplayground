324f082987a9f35d20a4aa0f6e0fcdc12d42099c
Jace Gonzales <jacegonzales25@gmail.com>
2023-05-18 22:54:16 +0800

    Added user input for each team player scores

--- BasketballGame.java
+++ BasketballGame.java
@@ -21,6 +21,35 @@ public class BasketballGame{
         String team2 = in.nextLine();
         System.out.println(team1 + " V.S " + team2);
 
+        int gameover = 10; //time of game
+		int score = 0;
+
+        System.out.println("Time of game " + gameover);
+		gameover --;
+		System.out.println("Which team scored: 1 or 2 ");
+        team = in.nextInt();
+
+        if(team == 1) {
+            System.out.println("Which player in team " +team1 + " scored? 1-12?");
+            player = in.nextInt();
+            if ((player >= 1 || player <= 12)){
+                System.out.print("Enter team " + team1 + " player " + player + " score: ");
+                score = in.nextInt(); 
+                playerscore[team-1][player-1] = playerscore[team-1][player-1] + score;
+            System.out.println(team1 + " player " + player + " score " + playerscore[team-1][player-1]);	
+            }
+        }
+        if (team == 2) {
+            System.out.println("Which player in team " + team2 + " scored? 1-12?");
+            player = in.nextInt();
+            if ((player >=1 || player <= 12)) {
+                System.out.print("Enter team " + team2 + " player " + player + " score: ");
+                score = in.nextInt();
+                playerscore[team-1][player-1] = playerscore[team-1][player-1] + score;
+            System.out.println(team2 + " player " + player + " score " + playerscore[team-1][player-1]);	
+            }
+        }
+
     }
 
 
