6c643c3810259fb08f446aacfd41b933bcf72b8d
Jace Gonzales <jacegonzales25@gmail.com>
2023-05-18 22:50:29 +0800

    Initialized Scanner, team and players, respective playerscores, user input of teams

--- BasketballGame.java
+++ BasketballGame.java
@@ -11,8 +11,20 @@ public class BasketballGame{
     }
 
     private static void playerDeterminance(){
+        Scanner in = new Scanner(System.in);
+        int team = 0, player = 0;
+        int playerscore[][] = new int[2][12];
+
+        System.out.println("Enter team 1 name: ");
+        String team1 = in.nextLine();
+        System.out.println("Enter team 2 name: ");
+        String team2 = in.nextLine();
+        System.out.println(team1 + " V.S " + team2);
+
+    }
 
 
+    private int teamScores(){
 
     }
 
