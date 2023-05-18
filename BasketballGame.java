5cb84a736720a682ba2e92d8c6f7ef45c27af085
Jace Gonzales <jacegonzales25@gmail.com>
2023-05-18 22:46:15 +0800

    Added Game Timer driver, Scoreboard 2d showcase

--- BasketballGame.java
+++ BasketballGame.java
@@ -1,8 +1,7 @@
 import java.util.*;
+
+// 2D array, 3 inputs, team-player-score, display result of game, loop while not game over
 public class BasketballGame{
-
-
-    
 
     public static void main(String[] args){
     // Driver Code
@@ -11,5 +10,25 @@ public class BasketballGame{
 
     }
 
+    private static void playerDeterminance(){
+
+
+
+    }
+
+
+    private int gameTimer(int gameTime){
+        return gameTime --;
+    }
+
+    private String playerScoreboard(String [][] playerscore){
+
+        for (int i = 0; i < 2; i++){
+            for (int j = 0; j < 12; j++){
+                return playerscore[i][j] + " ";
+            }
+        }
+
+    }
 
 }
